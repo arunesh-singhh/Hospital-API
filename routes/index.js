e595ddbb17c9b6309923842031936180a357a59e
Arunesh Singh <125174712+arunesh-singhh@users.noreply.github.com>
2024-05-06 08:48:51 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const router = express.Router();
 import doctorsRouter from './doctors.js';
 import patientsRouter from './patients.js';
 
+router.get('/', (req, res) => {
+    res.send("Welcome To Hospital API")
+});
 router.use('/api/doctors', doctorsRouter);
 router.use('/api/patients', patientsRouter);
 router.get('/api/reports/:status', passport.authenticate('jwt', { session: false }), filteredReports);
